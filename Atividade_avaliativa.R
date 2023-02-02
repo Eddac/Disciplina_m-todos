a8e5edebc48edc5fdba5378e2d9f6b77c0d97ad2
Eddac <cordeiro.emanuel@gmail.com>
2023-02-02 09:11:21 -0300

    Análise de dados

---
+++ Atividade_avaliativa.R
@@ -0,0 +1,19 @@
+# Atividade 1 (5 pontos)
+
+# A partir do banco de dados df_geral faça as seguintes tarefas:
+# Deixe apenas os números na coluna Idade, retirando a palavra "anos"
+# Confirme se a coluna Idade está númerica. Caso não, transforme em númerica. 
+# Crie os fatores de metas de realização que correspondem ao conjunto de itens específicos:
+# Fator 1 (Meta performance) - itens 58,59,60,61,64
+# Fator 2 (Meta aprender) - 62,63,65,66,67,68,69,70
+# Fator 3 (Meta evitação)- 71,72,73,74
+# Fator geral (Meta geral) - As três novas colunas
+# Lembrem que o fator é a média dos itens
+
+# Teste qui-quadrado de independência 
+# realize o teste comparando gênero e "Qual o seu curso"
+# Fique à vontade para usar o caminho que achar melhor
+# Além de escrever o script, escreva ou grave um curto vídeo explicando os resultados encontrados.
+
+
+
